c940079616eb98ef8b1b37764316a4e8716e64f8
Jacob <cph-js284@cphbusiness.dk>
2019-02-10 08:55:11 +0100

    Refactored the SettingUp-class extracted queries to a new Qs-class

---
+++ Qs.cs
@@ -0,0 +1,176 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using MongoDB.Driver;
+using mongoTwitter1.Models;
+
+namespace MongoDbQueries
+{
+    public class Qs
+    {
+        string DBname, CollectionName,ConnectionString;
+        Stopwatch stopwatch;
+        MongoClient client;
+        IMongoDatabase mdb;
+
+        public Qs()
+        {
+            DBname = "TwitterTextDb";
+            CollectionName ="TweetDocs";       
+            stopwatch = new Stopwatch();
+            client = new MongoClient(ConnectionString);
+            mdb = client.GetDatabase(DBname);
+        }
+
+                /*Question 1  */
+        public void CalculateUniqueUN(){
+            System.Console.WriteLine("Question 1) How many Twitter users are in the database?");
+            System.Console.WriteLine("Calculating unique number of usernames in Db - standby");
+            var collection = mdb.GetCollection<TwitterData>(CollectionName);
+
+            var UserNameCounter = collection.Distinct(x => x.UserName, _=> true).ToList().Count();
+
+            System.Console.WriteLine($"Number of unique username found in Db : {UserNameCounter}");
+            System.Console.WriteLine();
+        }
+
+        /*Question 2  */
+        public void CalculateUserLinkToOther(){
+            System.Console.WriteLine("Question 2) Which Twitter users link the most to other Twitter users? (Provide the top ten.)");            
+            System.Console.WriteLine("Calculating linking between users - standby");
+            var collection = mdb.GetCollection<TwitterData>(CollectionName);
+
+            var res = collection.AsQueryable()
+                    .Where(x => x.Text.Contains("@"))
+                    .GroupBy(u => u.UserName)
+                    .Select(usr => new{UserName = usr.Key, TweetCount = usr.Count()})
+                    .OrderByDescending(c => c.TweetCount)
+                    .Take(10);
+
+            foreach (var item in res)
+            {
+                System.Console.WriteLine($"User : {item.UserName} references other users {item.TweetCount} times");
+            }    
+            System.Console.WriteLine();
+        }
+
+        /*Question 3  */
+        public void CalculateMostMentioned(){
+            System.Console.WriteLine("Question 3) Who are the most mentioned Twitter users? (Provide the top five.)");
+            System.Console.WriteLine("Calculating most mentioned users - standby");
+            Dictionary<string, int> store = new Dictionary<string, int>();
+
+            var collection = mdb.GetCollection<TwitterData>(CollectionName);
+            var res = collection.AsQueryable().Where(x=>x.Text.Contains("@")).ToList();
+            
+            Regex regex = new Regex(@"@[\w]+");
+
+            foreach (var item in res)
+            {
+                var gr = regex.Match(item.Text).Groups;
+                    if(gr[0].Length>0){
+                        var tmp = gr[0].Value;
+                        if(store.TryGetValue(tmp, out int Counter)){
+                            store[tmp] = Counter + 1;
+                        }else{
+                            store[tmp] = 1;
+                        }
+                    }
+            }
+
+            var orderBy = store.OrderByDescending(x=>x.Value).Take(5);
+            foreach (var item in orderBy){
+                System.Console.WriteLine(item.Key +" - "+ item.Value);
+            }
+            System.Console.WriteLine();
+        }
+
+
+        /*Question 4  */
+        public void CalculateMostActive(){
+            System.Console.WriteLine("Question 4) Who are the most active Twitter users (top ten)?");
+            System.Console.WriteLine("Calculating most active twitter users - standby");
+            var collection = mdb.GetCollection<TwitterData>(CollectionName);
+
+            var res = collection.AsQueryable()
+                    .GroupBy(u => u.UserName)
+                    .Select(usr => new{UserName = usr.Key, TweetCount = usr.Count()})
+                    .OrderByDescending(c => c.TweetCount)
+                    .Take(10);
+
+            foreach (var item in res){
+                System.Console.WriteLine($"User : {item.UserName} has made {item.TweetCount} tweets");
+            }
+            System.Console.WriteLine();
+        }
+
+        /*Question 5  */
+        public void CalculateMostGrumpy(){
+            System.Console.WriteLine("Question 5) Who are the five most grumpy (most negative tweets) and the most happy (most positive tweets)?");
+            System.Console.WriteLine("Calculating most grumpy twitter users - standby");
+
+            var collection = mdb.GetCollection<TwitterData>(CollectionName);
+            var args = new AggregateOptions(){AllowDiskUse=true};
+            
+            var Full = collection.Aggregate(args)
+                    .Group(u=>u.UserName, grp=> new{
+                        UserName = grp.Key,
+                        PolScore = grp.Sum(y=>y.Polarity)
+                    }).ToList().OrderBy(x=>x.PolScore);
+
+            var Gres = Full.Take(5);
+            var Hres = Full.TakeLast(5);
+
+            System.Console.WriteLine("5 users with lowest polarity score (most grumpy)");
+            foreach (var item in Gres){
+                System.Console.WriteLine(item.UserName +" - - " + item.PolScore);
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("5 users with highest polarity score (least grumpy)");
+            foreach (var item in Hres.Reverse()){
+                System.Console.WriteLine(item.UserName +" - - " + item.PolScore);
+            }
+            System.Console.WriteLine();
+        }
+
+        /*method-wrapper; going through each query */
+        public void Questions(){
+            System.Console.WriteLine("--------------------------------------------------------------------");
+            System.Console.WriteLine("-------------- Setup done - running queries ------------------------");
+            System.Console.WriteLine("--------------------------------------------------------------------");
+            System.Console.WriteLine();
+
+            stopwatch.Start();
+            CalculateUniqueUN();
+            stopwatch.Stop();
+                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            CalculateUserLinkToOther();
+            stopwatch.Stop();
+                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            CalculateMostMentioned();
+            stopwatch.Stop();
+                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            CalculateMostActive();
+            stopwatch.Stop();
+                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            CalculateMostGrumpy();
+            stopwatch.Stop();
+                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
+            System.Console.WriteLine("Done... Press any key to exit program - This will also stop and remove (--rm) the docker container running this program");
+        }
+
+    }
+}
--- SettingUp.cs
+++ SettingUp.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text.RegularExpressions;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDbQueries;
 using mongoTwitter1.Models;
 
 namespace mongoTwitter1
@@ -14,9 +12,9 @@ namespace mongoTwitter1
     public class SettingUp
     {
         string Zipfile, CSVfile, DBname, CollectionName,host,port,ConnectionString;
-        Stopwatch stopwatch;
         MongoClient client;
         IMongoDatabase mdb;
+        Qs queries;
 
         public SettingUp()
         {
@@ -31,10 +29,9 @@ namespace mongoTwitter1
             host = DetectDockerBridgeGateWay();
             port = ":27017";
             ConnectionString = "mongodb://"+host+port;
-            stopwatch = new Stopwatch();
             client = new MongoClient(ConnectionString);
             mdb = client.GetDatabase(DBname);
-
+            queries = new Qs();
         }
 
         /*Downloading the zip from Stanford */
@@ -130,7 +127,8 @@ namespace mongoTwitter1
                     System.Console.WriteLine("All documents inserted");
                 }
             }
-            Questions();
+            BuildIndex();
+            queries.Questions();
         }
         /*Build indexes on UserName field */
         public void BuildIndex(){
@@ -142,154 +140,5 @@ namespace mongoTwitter1
             System.Console.WriteLine("Done building indexes");
         }
 
-        /*Question 1  */
-        public void CalculateUniqueUN(){
-            System.Console.WriteLine("Question 1) How many Twitter users are in the database?");
-            System.Console.WriteLine("Calculating unique number of usernames in Db - standby");
-            var collection = mdb.GetCollection<TwitterData>(CollectionName);
-
-            var UserNameCounter = collection.Distinct(x => x.UserName, _=> true).ToList().Count();
-
-            System.Console.WriteLine($"Number of unique username found in Db : {UserNameCounter}");
-            System.Console.WriteLine();
-        }
-
-        /*Question 2  */
-        public void CalculateUserLinkToOther(){
-            System.Console.WriteLine("Question 2) Which Twitter users link the most to other Twitter users? (Provide the top ten.)");            
-            System.Console.WriteLine("Calculating linking between users - standby");
-            var collection = mdb.GetCollection<TwitterData>(CollectionName);
-
-            var res = collection.AsQueryable()
-                    .Where(x => x.Text.Contains("@"))
-                    .GroupBy(u => u.UserName)
-                    .Select(usr => new{UserName = usr.Key, TweetCount = usr.Count()})
-                    .OrderByDescending(c => c.TweetCount)
-                    .Take(10);
-
-            foreach (var item in res)
-            {
-                System.Console.WriteLine($"User : {item.UserName} references other users {item.TweetCount} times");
-            }    
-            System.Console.WriteLine();
-        }
-
-        /*Question 3  */
-        public void CalculateMostMentioned(){
-            System.Console.WriteLine("Question 3) Who are the most mentioned Twitter users? (Provide the top five.)");
-            System.Console.WriteLine("Calculating most mentioned users - standby");
-            Dictionary<string, int> store = new Dictionary<string, int>();
-
-            var collection = mdb.GetCollection<TwitterData>(CollectionName);
-            var res = collection.AsQueryable().Where(x=>x.Text.Contains("@")).ToList();
-            
-            Regex regex = new Regex(@"@[\w]+");
-
-            foreach (var item in res)
-            {
-                var gr = regex.Match(item.Text).Groups;
-                    if(gr[0].Length>0){
-                        var tmp = gr[0].Value;
-                        if(store.TryGetValue(tmp, out int Counter)){
-                            store[tmp] = Counter + 1;
-                        }else{
-                            store[tmp] = 1;
-                        }
-                    }
-            }
-
-            var orderBy = store.OrderByDescending(x=>x.Value).Take(5);
-            foreach (var item in orderBy){
-                System.Console.WriteLine(item.Key +" - "+ item.Value);
-            }
-            System.Console.WriteLine();
-        }
-
-
-        /*Question 4  */
-        public void CalculateMostActive(){
-            System.Console.WriteLine("Question 4) Who are the most active Twitter users (top ten)?");
-            System.Console.WriteLine("Calculating most active twitter users - standby");
-            var collection = mdb.GetCollection<TwitterData>(CollectionName);
-
-            var res = collection.AsQueryable()
-                    .GroupBy(u => u.UserName)
-                    .Select(usr => new{UserName = usr.Key, TweetCount = usr.Count()})
-                    .OrderByDescending(c => c.TweetCount)
-                    .Take(10);
-
-            foreach (var item in res){
-                System.Console.WriteLine($"User : {item.UserName} has made {item.TweetCount} tweets");
-            }
-            System.Console.WriteLine();
-        }
-
-        /*Question 5  */
-        public void CalculateMostGrumpy(){
-            System.Console.WriteLine("Question 5) Who are the five most grumpy (most negative tweets) and the most happy (most positive tweets)?");
-            System.Console.WriteLine("Calculating most grumpy twitter users - standby");
-
-            var collection = mdb.GetCollection<TwitterData>(CollectionName);
-            var args = new AggregateOptions(){AllowDiskUse=true};
-            
-            var Full = collection.Aggregate(args)
-                    .Group(u=>u.UserName, grp=> new{
-                        UserName = grp.Key,
-                        PolScore = grp.Sum(y=>y.Polarity)
-                    }).ToList().OrderBy(x=>x.PolScore);
-
-            var Gres = Full.Take(5);
-            var Hres = Full.TakeLast(5);
-
-            System.Console.WriteLine("5 users with lowest polarity score (most grumpy)");
-            foreach (var item in Gres){
-                System.Console.WriteLine(item.UserName +" - - " + item.PolScore);
-            }
-            System.Console.WriteLine();
-            System.Console.WriteLine("5 users with highest polarity score (least grumpy)");
-            foreach (var item in Hres.Reverse()){
-                System.Console.WriteLine(item.UserName +" - - " + item.PolScore);
-            }
-            System.Console.WriteLine();
-        }
-
-        /*method-wrapper; going through each query */
-        public void Questions(){
-            BuildIndex();
-            System.Console.WriteLine("--------------------------------------------------------------------");
-            System.Console.WriteLine("-------------- Setup done - running queries ------------------------");
-            System.Console.WriteLine("--------------------------------------------------------------------");
-            System.Console.WriteLine();
-
-            stopwatch.Start();
-            CalculateUniqueUN();
-            stopwatch.Stop();
-                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
-            stopwatch.Reset();
-
-            stopwatch.Start();
-            CalculateUserLinkToOther();
-            stopwatch.Stop();
-                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
-            stopwatch.Reset();
-
-            stopwatch.Start();
-            CalculateMostMentioned();
-            stopwatch.Stop();
-                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
-            stopwatch.Reset();
-
-            stopwatch.Start();
-            CalculateMostActive();
-            stopwatch.Stop();
-                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
-            stopwatch.Reset();
-
-            stopwatch.Start();
-            CalculateMostGrumpy();
-            stopwatch.Stop();
-                System.Console.WriteLine($"Method completed in {stopwatch.ElapsedMilliseconds} ms\n\n");
-            System.Console.WriteLine("Done... Press any key to exit program - This will also stop and remove (--rm) the docker container running this program");
-        }
     }
 }
